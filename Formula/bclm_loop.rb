9517f7b2f688878bb4436be357321f4957f84275
lslqtz <admin@acgn.xyz>
2025-09-20 18:37:16 +0800

    Update bclm_loop.rb

--- Formula/bclm_loop.rb
+++ Formula/bclm_loop.rb
@@ -1,9 +1,9 @@
 class BclmLoop < Formula
 	desc "A background looping utility that maintains the battery level of Apple Silicon based Mac computers"
 	homepage "https://github.com/lslqtz/bclm_loop"
-	url "https://github.com/lslqtz/bclm_loop/releases/download/1.0b6/bclm_loop.zip"
-	version "1.0b6"
-	sha256 "e3a18bc4c13174bc0a459f74bbe4bd32d4496f98c161643292d7f9190972b715"
+	url "https://github.com/lslqtz/bclm_loop/releases/download/1.0b7/bclm_loop.zip"
+	version "1.0b7"
+	sha256 "01644198404cdf901fad9ca4a23e895ec5a010836d5f2ca76132ff0cc9dccc6c"
 	license "https://github.com/lslqtz/bclm_loop/blob/main/LICENSE"
 
 	def install
